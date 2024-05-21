117fede40549a65a0c2e2c430919842dda0f2432
tangwai <tmichael11314@gmail.com>
2024-05-21 17:28:41 -0500

    test delete/add src/ to player pic

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import ShuffleCards from './components/ShuffleCards.jsx';
 
 function App() {
   const initialPlayers = [
-    { id: 1, name: "Lebron James", picture: "src/player-pictures/lebron.png" },
-    { id: 2, name: "Nikola Jokic", picture: "src/player-pictures/jokic.png" },
-    { id: 3, name: "Kevin Durant", picture: "src/player-pictures/kd.png" },
-    { id: 4, name: "De'Aaron Fox", picture: "src/player-pictures/fox.png" },
-    { id: 5, name: "Shai Gilgeous-Alexander", picture: "src/player-pictures/shai.png" },
-    { id: 6, name: "Giannis Antetokounmpo", picture: "src/player-pictures/giannis.png" },
-    { id: 7, name: "Donovan Mitchell", picture: "src/player-pictures/mitchell.png" },
-    { id: 8, name: "Kyrie Irving", picture: "src/player-pictures/kyrie.png" },
-    { id: 9, name: "Luka Doncic", picture: "src/player-pictures/doncic.png" },
-    { id: 10, name: "Stephen Curry", picture: "src/player-pictures/curry.png" },
+    { id: 1, name: "Lebron James", picture: "/player-pictures/lebron.png" },
+    { id: 2, name: "Nikola Jokic", picture: "/player-pictures/jokic.png" },
+    { id: 3, name: "Kevin Durant", picture: "/player-pictures/kd.png" },
+    { id: 4, name: "De'Aaron Fox", picture: "/player-pictures/fox.png" },
+    { id: 5, name: "Shai Gilgeous-Alexander", picture: "/player-pictures/shai.png" },
+    { id: 6, name: "Giannis Antetokounmpo", picture: "/player-pictures/giannis.png" },
+    { id: 7, name: "Donovan Mitchell", picture: "/player-pictures/mitchell.png" },
+    { id: 8, name: "Kyrie Irving", picture: "/player-pictures/kyrie.png" },
+    { id: 9, name: "Luka Doncic", picture: "/player-pictures/doncic.png" },
+    { id: 10, name: "Stephen Curry", picture: "/player-pictures/curry.png" },
     { id: 11, name: "Jayson Tatum", picture: "src/player-pictures/tatum.png" },
   ];
 
